b2bb0c5fb559a2933c75c07fc191dbece469e5a3
Jephin John <jephinjohn85@gmail.com>
2021-08-30 20:46:31 +0530

    Added Data Binding

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -6,25 +6,28 @@ import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.*
+import androidx.databinding.DataBindingUtil
+import com.example.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val btnDone = findViewById<Button>(R.id.btn_Done)
-        btnDone.setOnClickListener{
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.btnDone.setOnClickListener{
             addName(it)
         }
     }
 
     private fun addName(btnDone : View){
-        val realName =findViewById<EditText>(R.id.real_name)
-        val invisibleText = findViewById<TextView>(R.id.invisible_text)
-        invisibleText.text = "Real Name :- "+realName.text
-        btnDone.visibility= View.GONE
-        realName.visibility = View.GONE
-        invisibleText.visibility = View.VISIBLE
-
+        binding.apply {
+            invisibleText.text = "Real Name :- " + realName.text
+            btnDone.visibility = View.GONE
+            realName.visibility = View.GONE
+            invisibleText.visibility = View.VISIBLE
+            invalidateAll()
+        }
         //Code to hide the keyboard
         val inputMethodMgr = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         inputMethodMgr.hideSoftInputFromWindow(btnDone.windowToken,0)
